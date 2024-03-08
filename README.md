e777b4b08df2d5290eca41e9f282fd961ebcb22a
Sanah Keloth <shedavalath-vayalil@myseneca.ca>
2024-03-08 03:23:26 +0000

    versioning complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CLO835-manifest
+
+kubectl get all -n webapp -l app.kubernetes.io/version=v1  kubectl get all -n webapp -l app.kubernetes.io/version=v2
+
+kubectl get all -n webapp -l app.kubernetes.io/name=myapp,app.kubernetes.io/component=frontend,app.kubernetes.io/part-of=employeeapp
+
+kubectl get pod mysql-pod -n sqldb -o jsonpath='{.status.podIP}'
--- cluster-deployment.sh
+++ cluster-deployment.sh
@@ -13,48 +13,80 @@ create_ns_secrets(){
 }
 
 start_pod(){
+  echo "Starting kind cluster..."
+  kind create cluster --config kind-nodeport.yaml
+  
   echo "Starting pods..."
+  
+  create_ns_secrets
+  
+  echo "Creating backend pod..."
+  kubectl apply -f kubManifest/backend-pod.yaml -n sqldb
+  
+  echo "Getting pod IP"
+  kubectl wait --for=condition=ready pod/mysql-pod -n sqldb --timeout=90s
+  pod_ip=$(kubectl get pod/mysql-pod -n sqldb -o jsonpath='{.status.podIP}')
+
+  echo "Creating frontend pod..."
+  sed "s/{{POD_IP}}/${pod_ip}/g" kubManifest/frontend-pod.yaml | kubectl apply -f - -n webapp
+
+  echo "Port forwarding..."
+  kubectl wait --for=condition=ready pod/myapp-v1-pod -n webapp --timeout=90s
+  kubectl port-forward --address 0.0.0.0 pod/myapp-v1-pod 30010:8080 -n webapp
+  echo "App avalaible at <ip>:30010"
 }
 
 start_replicaset(){
   echo "Starting replica sets..."
-}
-
-start_deployment() {
-  echo "Starting deployment..."
-
-  echo "Creating cluster..."
-  kind create cluster --config kind-nodeport.yaml
   
-  create_ns_secrets 
-
-  echo "Creating backend deployment..."
-  kubectl apply -f kubManifest/backend-deployment.yaml -n sqldb
+  echo "Creating backend pod..."
+  kubectl apply -f kubManifest/backend-replicaset.yaml -n sqldb
+  
   echo "Creating backend clusterIP service..."
   kubectl apply -f kubManifest/backend-service.yaml -n sqldb
   
-  
   echo "Creating frontend deployment..."
-  # For each app color version
-  sed 's/{{APP_COLOR}}/blue/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/pink/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
+  kubectl apply -f kubManifest/frontend-replicaset.yaml -n webapp
   
-  echo "Creating frontend clusterIP services..."
-  sed 's/{{APP_COLOR}}/blue/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/pink/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
+  echo "Creating frontend clusterIP service..."
+  kubectl apply -f kubManifest/frontend-service.yaml -n webapp
   
-  echo "Creating frontend NodePort services..."
-  
-  sed 's/{{APP_COLOR}}/blue/g; s/{{APP_CONTAINER}}/30010/g' kubManifest/frontend-NodePort.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/pink/g; s/{{APP_CONTAINER}}/30100/g' kubManifest/frontend-NodePort.yaml | kubectl apply -f - -n webapp
-  echo "Blue app avalaible at <ip>:30010"
-  echo "Pink app avalaible at <ip>:30100"
+  kubectl port-forward svc/myapp-v1-svc 30010:80 -n webapp
+  echo "App avalaible at <ip>:30010"
 }
 
-start_ingress() {
-  echo "Starting ingress..."
+start_nodeport_deployment() {
+  echo "Starting deployment using Nodeport service..."
+  
+  echo "Creating backend deployment..."
+  kubectl apply -f kubManifest/backend-deployment.yaml -n sqldb
+  
+  echo "Creating frontend deployment..."
+  sed 's/{{APP_VERSION}}/v1/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
+  
+  echo "Creating frontend NodePort services..."
+  sed 's/{{APP_VERSION}}/v1/g; s/{{APP_CONTAINER}}/30010/g' kubManifest/frontend-NodePort.yaml | kubectl apply -f - -n webapp
+  echo "App avalaible at <ip>:30010"
+}
 
-  echo "Creating cluster..."
+start_nodeport_version() {
+  echo "Starting versioning using Nodeport service..."
+  
+  echo "Creating frontend deployment for v2..."
+  sed 's/{{APP_VERSION}}/v2/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
+  
+  echo "Creating frontend NodePort services for v2..."
+  sed 's/{{APP_VERSION}}/v2/g; s/{{APP_CONTAINER}}/30100/g' kubManifest/frontend-NodePort.yaml | kubectl apply -f - -n webapp
+  echo "Version2 app avalaible at <ip>:30100"
+}
+
+start_ingress_version() {
+  delete_resources
+  delete_cluster
+  
+  echo "Starting versions using Ingress..."
+
+  echo "Starting Ingress cluster..."
   kind create cluster --config kind-ingress.yaml
  
   create_ns_secrets 
@@ -65,15 +97,12 @@ start_ingress() {
   kubectl apply -f kubManifest/backend-service.yaml -n sqldb
   
   echo "Creating frontend deployment..."
-  # For each app color version
-  sed 's/{{APP_COLOR}}/blue/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/pink/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/red/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
+  sed 's/{{APP_VERSION}}/v1/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
+  sed 's/{{APP_VERSION}}/v2/g' kubManifest/frontend-deployment.yaml | kubectl apply -f - -n webapp
   
   echo "Creating frontend clusterIP services..."
-  sed 's/{{APP_COLOR}}/blue/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/pink/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
-  sed 's/{{APP_COLOR}}/red/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
+  sed 's/{{APP_VERSION}}/v1/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
+  sed 's/{{APP_VERSION}}/v2/g' kubManifest/frontend-service.yaml | kubectl apply -f - -n webapp
 
   echo "Creating ingress controller..."
   kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
@@ -81,13 +110,12 @@ start_ingress() {
 
   echo "Creating frontend ingress service..."
   kubectl apply -f kubManifest/frontend-ingress.yaml -n webapp
-  echo "Blue app avalaible at <ip>/blue"
-  echo "Pink app avalaible at <ip>/pink"
-  echo "Red app defaults at <ip>"
+  echo "Version1 avalaible at <ip>/v1"
+  echo "Version2 avalaible at <ip>/v2"
 }
 
-start_versioning_ingress() {
-  echo "Starting versioning with ingress..."
+start_ingress_version_color() {
+  echo "Starting version and color routing with ingress..."
 }
 
 delete_resources() {
@@ -96,22 +124,27 @@ delete_resources() {
   kubectl delete all --all -n sqldb
   kubectl delete namespace webapp
   kubectl delete namespace sqldb
-  kubectl delete all --all
 }
 
-stop_and_cleanup() {
+delete_cluster() {
   echo "Deleting cluster..."
   kind delete cluster
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q)
+  docker rmi $(docker images -q)
+  docker volume rm $(docker volume ls -q)
+  docker network rm $(docker network ls -q)
 }
 
 echo "Select an action:"
 echo "1. Pods"
 echo "2. Replica Sets"
-echo "3. Deployments"
-echo "4. Ingress"
-echo "5. Versioning using Ingress"
-echo "6. Delete Resources"
-echo "7. Delete Cluster"
+echo "3. Deployment"
+echo "4. Nodeport - Version"
+echo "5. Ingress - Version"
+echo "6. Ingress - Version & Color"
+echo "7. Delete Resources"
+echo "8. Delete Cluster"
 read -p "Enter your choice (1/2): " choice
 
 case "$choice" in
@@ -125,13 +158,19 @@ case "$choice" in
     start_deployment
     ;;
   "4")
-    start_ingress
+    start_nodeport_version
     ;;
   "5")
-    start_versioning_ingress
+    start_ingress_version
     ;;
   "6")
-    stop_and_cleanup
+    start_ingress_version_color
+    ;;
+  "7")
+    delete_resources
+    ;;
+  "8")
+    delete_cluster
     ;;
   *)
     echo "Invalid choice. Please select 1 - 6."
